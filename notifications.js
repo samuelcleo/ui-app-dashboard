b6d6c027149541e5d1ddfd6395c96b8892735549
samuelcleo <cleophassamuel@gmail.com>
2023-02-28 08:23:16 +1100

    Add Notification Dot, Angle Quotes
    - Add and style notification dot above bell SVG
    - Add right angle quotation-mark as pseudo element arrow for each activity list item.

--- notifications.js
+++ notifications.js
@@ -32,7 +32,7 @@ alertBanner.innerHTML =
     `<div class="alert-banner">
     <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
     to complete</p>
-    <p class="alert-banner-close">x</p>
+    <p class="alert-banner-close">&#215</p>
     </div>
     `
 
